429a714680cd6f47325f8da6443b3f388142c9d8
jvcaetano123 <joaovitorcaetano81633@gmail.com>
2025-07-29 10:49:12 -0300

    EntradadeDados.dart

---
+++ EntradadeDados.dart
@@ -0,0 +1,14 @@
+void main(){
+  int idade = 25;
+  String nome = 'joâo';
+  double altura = 1.75;
+  double salario = 2500.50;
+  
+  List listadecarros = ['FUSCA,CORSA,GOL,UNO'];
+
+  print(nome);
+  print(idade);
+  print(altura);
+  print(salario);
+  print(listadecarros);
+}
